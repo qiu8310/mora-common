4a58bb6874665dae71e377364bf68ecd67c062dd
Mora <qiuzhongleiabc@126.com>
2017-08-15 19:03:11 +0800

    onview 支持指定 container

--- src/dom/onview.ts
+++ src/dom/onview.ts
@@ -2,7 +2,7 @@ import {throttle as throttleCall, debounce as debounceCall} from '../util/delay'
 import onload from './onload'
 
 // debounce & throttle: http://drupalmotion.com/article/debounce-and-throttle-visual-explanation
-export default function onview(fn: () => void, {runOnLoaded = true, resize = true, scroll = true, throttle = 0, debounce = 0} = {}): () => void {
+export default function onview(fn: () => void, {runOnLoaded = true, resize = true, scroll = true, container = null, throttle = 0, debounce = 0} = {}): () => void {
   let cb
 
   if (throttle && throttle > 0) {
@@ -17,9 +17,11 @@ export default function onview(fn: () => void, {runOnLoaded = true, resize = tru
 
   if (resize) window.addEventListener('resize', cb)
   if (scroll) window.addEventListener('scroll', cb)
+  if (container && scroll) container.addEventListener('scroll', cb)
 
   return () => {
     if (resize) window.removeEventListener('resize', cb)
     if (scroll) window.removeEventListener('scroll', cb)
+    if (container && scroll) container.removeEventListener('scroll', cb)
   }
 }
