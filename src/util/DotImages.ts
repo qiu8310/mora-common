6c9093352f559e0a6ef715f2ff896d91faf17ed0
Mora <qiuzhongleiabc@126.com>
2017-08-16 14:14:54 +0800

    添加 DotImages

---
+++ src/util/DotImages.ts
@@ -0,0 +1,3 @@
+/* tslint:disable:variable-name */
+export const WhiteDotImage = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+export const BlackDotImage = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
