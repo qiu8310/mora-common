1df6f22523455b00eb498effa684dfc9dddf1a3d
Mora <qiuzhongleiabc@126.com>
2017-08-30 16:49:20 +0800

    update

--- src/util/prettyBytes.ts
+++ src/util/prettyBytes.ts
@@ -12,7 +12,7 @@ export default function prettyBypes(num, base = 1024) {
   let exponent = 0
   let testNum = num
   let found = UNITS.some((unit, index) => {
-    testNum /= index ? 1024 : 1
+    testNum /= index ? base : 1
     if (testNum < 1) {
       exponent = index - 1
       return true
@@ -22,7 +22,6 @@ export default function prettyBypes(num, base = 1024) {
 
   // const exponent = Math.min(Math.floor(Math.log10(num) / 3), UNITS.length - 1)
   const numStr = Number((num / Math.pow(base, exponent)).toPrecision(3))
-  const unit = UNITS[exponent]
 
-  return (neg ? '-' : '') + numStr + ' ' + unit
+  return (neg ? '-' : '') + numStr + ' ' + UNITS[exponent]
 }
